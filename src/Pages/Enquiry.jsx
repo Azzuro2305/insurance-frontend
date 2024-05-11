f1dd798a51395abfd82e14d0e5f21cba02d05397
JiJi9526 <maynine952006@gmail.com>
2024-05-11 12:02:13 +0630

    enquiry

--- src/Pages/Enquiry.jsx
+++ src/Pages/Enquiry.jsx
@@ -5,7 +5,7 @@ import Report from '../Components/EnquirySearch/Report'
 const Enquiry = () => {
   return (
     <div className=' shadow-xl my-10'>
-        <div className='p-12'>
+        <div className='xs2:p-5 md:p-12'>
             <PassportSearch/>
             <Report/>
         </div>
